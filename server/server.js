047683bbe1bd2ac422935721976a25f5bfd31098
snigdhaydv27 <snigdhak.ug23.cs@nitp.ac.in>
2025-11-09 11:45:17 +0530

    port changes

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send('Hello from the server!');
